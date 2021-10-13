dc4e07f8740ef571d60aec392e2833f9000d817f
Dimitri Williams <dwilliams_64@yahoo.com>
2021-10-13 18:40:51 -0500

    explored portals

---
+++ 08-finished/src/components/UI/ErrorModal.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { useRef } from "react";
+
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
+
+const BackDrop = (props) => {
+  console.log();
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const Modal = (props) => {
+  const paragraph = useRef();
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p ref={paragraph}>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop")
+      )}
+
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay")
+      )}
+    </>
+  );
+};
+
+export default ErrorModal;
