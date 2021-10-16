e2744f87eb4a078842a826a97fb436d6590c35d2
Dimitri Williams <dwilliams_64@yahoo.com>
2021-10-16 13:35:15 -0500

    Refactored to use context to manage auth state, created input component

--- section-10/src/components/Login/Login.js
+++ section-10/src/components/Login/Login.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 import {
   EMAIL_INITIAL_STATE,
   emailReducer,
 } from "../../reducers/email-reducer";
-
 import {
   PASSWORD_INITIAL_STATE,
   passwordReducer,
@@ -12,12 +11,12 @@ import {
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+import AuthContext from "../../store/auth-context";
 
-const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState("");
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState("");
-  // const [passwordIsValid, setPasswordIsValid] = useState();
+const Login = () => {
+  const ctx = useContext(AuthContext);
+
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, emailDispatch] = useReducer(
@@ -46,7 +45,6 @@ const Login = (props) => {
   }, [isEmailValid, isPasswordValid]);
 
   const emailChangeHandler = (event) => {
-    // setEnteredEmail(event.target.value);
     emailDispatch({
       type: "SET_VALUE",
       payload: { value: event.target.value },
@@ -54,7 +52,6 @@ const Login = (props) => {
   };
 
   const passwordChangeHandler = (event) => {
-    // setEnteredPassword(event.target.value);
     passwordDispatch({
       type: "SET_VALUE",
       payload: { value: event.target.value },
@@ -62,14 +59,12 @@ const Login = (props) => {
   };
 
   const validateEmailHandler = () => {
-    // setEmailIsValid(enteredEmail.includes("@"));
     emailDispatch({
       type: "CHECK_VALID",
     });
   };
 
   const validatePasswordHandler = () => {
-    // setPasswordIsValid(enteredPassword.trim().length > 6);
     passwordDispatch({
       type: "CHECK_VALID",
     });
@@ -77,40 +72,32 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          isValid={emailState.isValid}
+          label="E-Mail"
+          type="email"
+          id="email"
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+
+        <Input
+          isValid={passwordState.isValid}
+          label="Password"
+          type="password"
+          id="password"
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
+
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
--- section-10/src/store/auth-context.js
+++ section-10/src/store/auth-context.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogin: (email, password) => {},
+  onLogout: () => {},
 });
+
+export const AuthProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const isUserLogin = localStorage.getItem("isUserLogin");
+
+    if (isUserLogin) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isUserLogin", true);
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.setItem("isUserLogin", false);
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
