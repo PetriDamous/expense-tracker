abef8175de9f1d8a17df08b2f1d43ac7f374809d
dwilliams64 <dwilliams_64@yahoo.com>
2021-10-19 20:43:21 -0500

    setup redux and subscription to store

---
+++ redux-in-react/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore } from "redux";
+
+export const increment = () => ({ type: "INCREMENT" });
+
+export const decrement = () => ({ type: "DECREMENT" });
+
+const countReducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(countReducer);
+
+export default store;
