eb08a897317f3f1ea9905673219620f2a5ba5c3a
dwilliams64 <dwilliams_64@yahoo.com>
2021-10-20 19:42:25 -0500

    wired up action dispatches

--- redux-in-react/src/components/Counter.js
+++ redux-in-react/src/components/Counter.js
@@ -1,8 +1,16 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
+import { increment, decrement } from "../store";
+
+import Button from "./Button/Button";
 
 const Counter = () => {
   const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => dispatch(increment());
+
+  const handleDecrement = () => dispatch(decrement());
 
   const toggleCounterHandler = () => {};
 
@@ -10,7 +18,11 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{count}</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div>
+        <Button onClick={handleIncrement}>increment +</Button>
+        <Button onClick={handleDecrement}>decrement -</Button>
+      </div>
+      <Button onClick={toggleCounterHandler}>Toggle Counter</Button>
     </main>
   );
 };
