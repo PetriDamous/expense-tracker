545a046bd4b443de9fb48900036cd3c7906c4856
dwilliams64 <dwilliams_64@yahoo.com>
2021-10-26 19:33:50 -0500

    Explored useMemo(), useCallback(), component evaluation, state scheduling, optimization tech

--- section-12-opti-tech/src/App.js
+++ section-12-opti-tech/src/App.js
@@ -8,25 +8,51 @@ import "./App.css";
 function App() {
   const [text, setText] = useState(false);
 
-  console.log("APP_RAN");
+  console.log("APP_ran");
+
+  // randomState will be a variable that we will
+  // use inside of useCallback().
+  const [randomState, setRandomState] = useState(false);
 
   const handleToggle = useCallback(() => {
-    const str = "abcdefghijklmnopqrstuvwxyz";
+    // randomState will be false the first time the Component is mounted.
+    // This value will stay false even on re-evaluation.
+    // The function in useCallback() encloses the randomState variable.
+    // It will now always be the same since useCallback() is used to store a
+    // function in memory and prevent this function from being re-created
+    console.log(randomState);
 
-    let newStr = "";
+    // Since this value is always false this conditional block never runs.
+    if (randomState) {
+      const str = "abcdefghijklmnopqrstuvwxyz";
 
-    for (let i = 0; i < 5; i++) {
-      const randomNum = Math.floor(Math.random() * str.length);
-      newStr += str[randomNum];
+      let newStr = "";
+
+      for (let i = 0; i < 5; i++) {
+        const randomNum = Math.floor(Math.random() * str.length);
+        newStr += str[randomNum];
+      }
+
+      setText(newStr);
     }
+  }, [randomState]);
 
-    setText(newStr);
-  }, []);
+  const handleRandomState = () => {
+    setRandomState((prev) => !prev);
+  };
+
+  // When handleRandomState runs the value of randomState will change.
+  // The same value inside of the function passed to useCallback() will never
+  // change because useCallback() is used to store that fucntion in memory
+  // so the function is never re-created.  It is still the same function holding
+  // the false value for randomState from when it first ran.
+  console.log(randomState);
 
   return (
     <div className="app">
       {<h1>Hi there!</h1>}
-      <Demo text={false} />
+      <Demo text={text} />
+      <Button onClick={handleRandomState}>Toggle Random State</Button>
       <Button onClick={handleToggle}>Toggle Paragraph</Button>
     </div>
   );
