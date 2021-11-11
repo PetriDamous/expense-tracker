1c3d29796d0517aa172ba53512c4c411deacb28f
dwilliams64 <dwilliams_64@yahoo.com>
2021-11-10 21:59:50 -0600

    Added cart button animation

--- section-11-food/food-order/src/components/Layout/cart-button/CartButton.js
+++ section-11-food/food-order/src/components/Layout/cart-button/CartButton.js
@@ -1,15 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import "remixicon/fonts/remixicon.css";
 import classes from "./styles/cart-button.module.css";
 
 import CartContext from "../../../store/cart/cart-context";
 
+let isInitialLoad = true;
+
 const CartButton = ({ handleShowCart }) => {
-  const { totalQuantity } = useContext(CartContext);
+  const [isAnimationOn, setAnimationOn] = useState(false);
+
+  const { cartItems, totalQuantity } = useContext(CartContext);
+
+  useEffect(() => {
+    if (isInitialLoad) {
+      isInitialLoad = false;
+      return;
+    }
+
+    setAnimationOn(true);
+
+    const timeId = setTimeout(() => {
+      setAnimationOn(false);
+    }, 300);
+
+    return () => clearTimeout(timeId);
+  }, [cartItems]);
+
+  const btnClasses = `${classes.button} ${isAnimationOn && classes.bump}`;
 
   return (
-    <button className={classes.button} onClick={handleShowCart}>
+    <button className={btnClasses} onClick={handleShowCart}>
       <span className={classes.icon}>
         <i class="ri-shopping-cart-line"></i>
       </span>
