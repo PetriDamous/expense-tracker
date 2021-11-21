eeccd73a52dd630b36f6a911200be2ae38a59010
dwilliams64 <dwilliams_64@yahoo.com>
2021-11-21 17:27:19 -0600

    Setup routes for app

---
+++ section-20-react-router/new-project/src/pages/QuoteDetail.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, useParams } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail = () => {
+  const { quoteId } = useParams();
+
+  return (
+    <>
+      <h1>QuoteDetail Page</h1>
+      <p>Product: {quoteId}</p>
+      <Route path={`/quotes/:${quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetail;
